4253f4afcab90568b5585576e0a95cf9c3b0e134
Ethan Gordon <ekgordon2@gmail.com>
2015-04-12 01:11:31 -0400

    Added audio package

---
+++ src/main/java/kouyang/irlsubs/audio/MainAudio.java
@@ -0,0 +1,112 @@
+package kouyang.irlsubs.audio;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.TargetDataLine;
+
+public class MainAudio extends Thread {
+	// Constants
+	// Determine Audio Format (44.1 kHz, 16-bit signed, stereo)
+	final static float fSampleRate = 48000.0F; // 16 kHz Sample Rate
+	final static int iBytesPerSample = 2; // 16-bit sample
+	final static int channels       = 2;  // Stereo Recording
+	final static int frameSize = channels * iBytesPerSample;
+	
+	final static int CHUNK_SIZE = 1024;
+
+	private AudioFormat m_format;
+	private PipedInputStream[] m_inStreams;
+	private PipedOutputStream[] m_outStreams;
+	private TargetDataLine microphone;
+	
+	private boolean m_stopped;
+	
+	public MainAudio(int numStreams) {
+		m_format = new AudioFormat(
+				AudioFormat.Encoding.PCM_SIGNED,
+				fSampleRate, iBytesPerSample * 8, channels, frameSize, fSampleRate, false);
+		m_inStreams = new PipedInputStream[numStreams];
+		m_outStreams = new PipedOutputStream[numStreams];
+		
+		for(int i = 0; i < m_inStreams.length; i++) {
+			try {
+				m_inStreams[i] = new PipedInputStream();
+				m_outStreams[i] = new PipedOutputStream(m_inStreams[i]);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+		
+		m_stopped = false;
+	}
+	
+	public PipedInputStream[] initialize() {
+		try {
+			microphone = AudioSystem.getTargetDataLine(m_format);
+	
+			DataLine.Info info = new DataLine.Info(TargetDataLine.class, m_format);
+			microphone = (TargetDataLine) AudioSystem.getLine(info);
+			microphone.open(m_format);
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		return m_inStreams;
+	}
+	
+	public void quit() {
+		m_stopped = true;
+	}
+	
+	@Override
+	public void run() {
+		int numBytesRead;
+		
+		byte[] data = new byte[microphone.getBufferSize() / 5];
+		
+		microphone.start();
+		try {
+			while (!m_stopped) {
+				numBytesRead = microphone.read(data, 0, CHUNK_SIZE);
+				// write the microphone data to a stream for use later
+				for(PipedOutputStream p : m_outStreams) {
+					p.write(data, 0, numBytesRead); 
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		microphone.close();
+	}
+	
+	public static void main(String[] args) {
+		MainAudio m = new MainAudio(1);
+		
+		PipedInputStream[] streams = m.initialize();
+		
+		OffsetCalc oc = new OffsetCalc(0.0762, 1.0, streams[0]);
+		
+		m.start();
+		oc.start();
+		try {
+			while(System.in.available() < 1) {
+				Thread.sleep(1000);
+				System.out.println("Angle: " + oc.GetSourceAngle());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			oc.quit();
+			m.quit();
+		}
+		System.out.println("Ending...");
+	}
+
+}
---
+++ src/main/java/kouyang/irlsubs/audio/OffsetCalc.java
@@ -0,0 +1,141 @@
+package kouyang.irlsubs.audio;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+
+import org.apache.commons.math3.complex.Complex;
+import org.apache.commons.math3.transform.*;
+
+public class OffsetCalc extends Thread {
+	
+	final static int CHUNK_SIZE = 1024;
+	
+	final static double SOUND_SPEED = 342.2; // SI Units
+	
+	// Fields
+	private double m_micDistance;
+	private double m_refresh;
+	
+	private double m_offset;
+	
+	private boolean m_stopped;
+	
+	private Lock lock;
+
+	PipedInputStream m_audioData;
+
+
+	public OffsetCalc(double micDistance, double refresh, PipedInputStream audioData) {
+		m_refresh = refresh;
+		m_micDistance = micDistance;
+		m_audioData = audioData;
+		m_offset = 0;
+		
+		lock = new ReentrantLock();
+		m_stopped = false;
+	}
+	
+	public void quit() {
+		m_stopped = true;
+	}
+
+	@Override
+	public void run() {
+		while(!m_stopped) {
+			
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+	
+			byte[] data = new byte[CHUNK_SIZE];
+			int bytesRead = 0;
+			int numBytesRead = 0;
+			
+			try {
+				while (bytesRead < (int)(m_refresh * MainAudio.fSampleRate * MainAudio.frameSize)) {
+					numBytesRead = m_audioData.read(data, 0, CHUNK_SIZE);
+					bytesRead += numBytesRead;
+					// write the microphone data to a stream for use later
+					out.write(data, 0, numBytesRead);
+					if (m_stopped) return;
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			
+			byte[] soundBytes = out.toByteArray();
+			short[] soundSamples = new short[soundBytes.length / 2];
+	
+			ByteBuffer.wrap(soundBytes).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(soundSamples);
+	
+			double[] leftSamples = new double[soundSamples.length + 34816];
+			double[] rightSamples = new double[soundSamples.length + 34816];
+	
+			for(int i = 0; i < soundSamples.length / 2; i++) {
+				leftSamples[i] = (double)soundSamples[2 * i];
+				rightSamples[i] = (double)soundSamples[2 * i + 1];
+			}
+	
+	
+	
+			// Start Fourier Transform
+			FastFourierTransformer fft = new FastFourierTransformer(DftNormalization.STANDARD);
+	
+			Complex[] leftTransform = fft.transform(leftSamples, TransformType.FORWARD);
+			Complex[] rightTransform = fft.transform(rightSamples, TransformType.FORWARD);
+	
+			Complex[] leftcorrelateTransform = new Complex[leftTransform.length];
+	
+			for(int i = 0; i < leftTransform.length; i++) {
+				leftcorrelateTransform[i] = leftTransform[i].conjugate().multiply(rightTransform[i]);
+			}
+	
+			Complex[] leftcorrelationComplex = fft.transform(leftcorrelateTransform, TransformType.INVERSE);
+	
+			double[] leftcorrelation = new double[leftcorrelationComplex.length];
+	
+			for(int i = 0; i < leftcorrelation.length; i++) {
+				leftcorrelation[i] = leftcorrelationComplex[i].abs();
+			}
+	
+			int leftmax = 0;
+			double leftbest = 0;
+	
+			for(int i = 0; i < leftcorrelation.length; i++) {
+				if (leftcorrelation[i] > leftbest) {
+					leftbest = leftcorrelation[i];
+					leftmax = i;
+				}
+			}
+	
+			if (leftmax > leftcorrelation.length / 2) leftmax -= leftcorrelation.length;
+	
+			lock.lock();
+		    try {
+		    	m_offset = (double)leftmax / (double)MainAudio.fSampleRate;
+		    } finally {
+		    	lock.unlock();
+		    }
+		}
+	}
+	
+	public double GetSourceAngle() {
+		double ret = 0.0;
+		lock.lock();
+	    try {
+	    	double ratio = m_offset * SOUND_SPEED / m_micDistance;
+	    	if (ratio > 1) ratio = 1;
+	    	if (ratio < -1) ratio = -1;
+	    	ret = Math.toDegrees(Math.asin(ratio));
+	    } finally {
+	    	lock.unlock();
+	    }
+	    return ret;
+	}
+}
